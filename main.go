827933e468bc09bae10b0f2c0d65b19ee8e128f4
bam-bam-bigelow <46587439+bam-bam-bigelow@users.noreply.github.com>
2024-09-03 14:19:46 +0200

    Add support for custom port from command-line args
    Previously, the server was hardcoded to port 3000. This update allows users to specify a port via a command-line argument, improving the flexibility of the server configuration. The default port remains 3000 if no argument is provided.

--- main.go
+++ main.go
@@ -3,14 +3,21 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fs)
 
-	log.Println("Listening on :3000...")
-	err := http.ListenAndServe(":3000", nil)
+	// get port from param
+	port := "3000"
+	if len(os.Args) > 1 {
+		port = os.Args[1]
+	}
+
+	log.Println("Listening on :" + port)
+	err := http.ListenAndServe(":"+port, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
